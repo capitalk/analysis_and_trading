9f873507dbf22c34dd01ef7af6d3471978e2bcbf
Timir <tkaria@capitalkpartners.com>
2011-09-21 19:01:20 +0000

    MIC utilities

--- aws_pipeline/mics.py
+++ aws_pipeline/mics.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python
 
 MIC_LIST = ["FXCM", "FXAL", "GAIN", "HSFX", "XCDE", "XCUR"]
+MIC_DICT = {'FXCM':'FXCM', 'FXAL': 'FXAll', 'GAIN':'Gain Capital', 'HSFX':'Hotspot', 'XCDE':'Baxter', 'XCUR':'Currenex'}
