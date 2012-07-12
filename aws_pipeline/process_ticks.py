1959f93dcf4ae7c1dc75a900458f752318461117
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-07-12 19:52:26 +0200

    fixed and updated worker code

--- aws_pipeline/process_ticks.py
+++ aws_pipeline/process_ticks.py
@@ -5,10 +5,6 @@ Usage:
 
 """
 import os
-import sys
-import glob
-import subprocess
-import errno
 import commands
 from optparse import OptionParser
 import s3_multipart_upload
@@ -17,22 +13,10 @@ import boto
 import mics
 import time
 import h5py
-from mics import MIC_LIST
 from boto.sqs.message import MHMessage
 
-CommandExtractFiles = """python /home/ec2-user/analysis_and_trading/feature_extraction/extractFeatures.py -d /home/ec2-user/features/ /home/ec2-user/%s"""
-CommandFileToS3 = """source /home/ec2-user/.bash_profile && python /home/ec2-user/analysis_and_trading/aws_pipeline/s3_multipart_upload.py  %s %s"""
-
-USE_EPHEMERAL = False
-
-# First ephemeral storage mount - MUST HAVE TRAILING SLASH
-if USE_EPHEMERAL:
-    EPHEMERAL0="/media/ephemeral0/"
-else:
-    EPHEMERAL0="/home/ec2-user/"
-
-# Feature output directory - MUST HAVE TRAILING SLASH
-FEATURE_DIR=EPHEMERAL0+"features/"
+feature_extrator = "~/analysis_and_trading/feature_extraction/extractFeatures.py"
+FEATURE_DIR = STORAGE_PREFIX + "features/"
 
 
 BUCKET_PREFIX="capk-"
@@ -62,8 +46,20 @@ if __name__ == "__main__":
     parser.add_option("-o", "--outqueue", dest="outqueue", help="SQS outbound queue name", default='outq')
     parser.add_option("-i", "--inqueue", dest="inqueue", help="SQS inbound queue name", default='inq')
     parser.add_option("-d", "--debug", dest="debug", help="Print debug information", default='False')
+    parser.add_option("-t", "--terminate", dest="terminate", help="Terminate instance when done", default=False)
+    parser.add_option("-e", "--ephemeral", dest="ephemeral", help="Use ephemeral storage on EC2", default=False)
+    parser.add_option("-r", "--max_retries", dest="max_retries", help="max number of retries before we accept queue is empty", default=3)
+    parser.add_option("-w", "--retry_wait", dest="retry_wait", "time to sleep between retries", default=10)
+
     (options, args) = parser.parse_args()
 
+    # First ephemeral storage mount - MUST HAVE TRAILING SLASH
+    if options.ephemeral:
+      STORAGE_PREFIX = "/media/ephemeral0/"
+    else:
+      STORAGE_PREFIX = "/home/ec2-user/"
+
+    # Feature output directory - MUST HAVE TRAILING SLASH
     print "Reading from queue: ", options.inqueue
     print "Writing to queue: ", options.outqueue
 
@@ -73,64 +69,57 @@ if __name__ == "__main__":
     qout = sqscxn.create_queue(options.outqueue)
     qin.set_message_class(MHMessage)
     qout.set_message_class(MHMessage)
-    retrys = 0
-    retry_wait = 10
-    extractOK = False
-    moveOK = False
-    while retrys < 10:
-        rs = qin.get_messages(visibility_timeout=60*30)
-        if len(rs) >= 1:
-            m = MHMessage()
-            m = rs[0] 
+    retries = 0
+    while retries < options.max_retries:
+        result_set = qin.get_messages(1, visibility_timeout=60*30)
+        if len(result_set) >= 1:
+            m = result_set[0] 
             print "Received message: ", m.get_body()
             input_file = m['input_file'] 
             bucket = m['bucket'] 
-            s3_download_file.get_s3_file_to_local(s3cxn, bucket,input_file, input_file, EPHEMERAL0)	
+            s3_download_file.get_s3_file_to_local(s3cxn, bucket, input_file, input_file, STORAGE_PREFIX)	
 
+            full_input_path = STORAGE_PREFIX + input_file
             hdf_file = input_file.replace('csv.gz', 'hdf')
-            hdf_path = FEATURE_DIR + hdf_file;
-            if os.path.isfile(FEATURE_DIR+hdf_file) and hdf_complete(FEATURE_DIR+hdf_file):
+            full_hdf_path = FEATURE_DIR + hdf_file
+            if os.path.isfile(full_hdf_path) and hdf_complete(full_hdf_path):
                 print "HDF generated and complete - skipping feature extraction" 
-                extractOK = True
             else:
                 print "Processing file: ", input_file
-                command = CommandExtractFiles % (input_file)
+                command = "python %s -d %s %s" % \
+                  (feature_extractor, FEATURE_DIR, full_input_path) 
                 (code, string) = commands.getstatusoutput(command) 
-                #extractOK = (code == 0)
                 if options.debug is True:
                     print "Processing retured: ", code, string
 
             print "Moving processed file file to bucket"
             # KTK - TODO - should wrap in try except block - to catch failed upload
-            s3_multipart_upload.main(FEATURE_DIR+hdf_file, bucket)
-            #command = CommandFileToS3 % (FEATURE_DIR+hdf_file, bucket)
-            #(code, string) = commands.getstatusoutput(command) 
-            #moveOK = (code == 0)
-            #if options.debug is True:
-            #    print "Move tick files returned: ", code, string
-            
-            #if extractOK and moveOK:
-            retrys = 0
+            s3_multipart_upload.main(full_hdf_path, bucket)
+           
+            retries = 0
             md = boto.utils.get_instance_metadata()
             m['instance-id'] = md['instance-id']
             m['public-hostname'] = md['public-hostname']
             m['completion-time'] =  time.asctime(time.gmtime())
             qout.write(m) 
             qin.delete_message(m)
-            os.remove(FEATURE_DIR+hdf_file)
+            os.remove(full_hdf_path)
+            os.remove(full_input_path)
                  
         else:
-            time.sleep(retry_wait)
-            retrys += 1
+            time.sleep(options.retry_wait)
+            retries += 1
      
-        if retrys == 10:
+        if retries == options.max_retries:
             md = boto.utils.get_instance_metadata()
             ec2cxn = boto.connect_ec2()
             m = MHMessage()
             m['shutdown-time'] = time.asctime(time.gmtime()) 
             m['instance-id'] = md['instance-id']
             m['public-hostname'] = md['public-hostname']
-            ec2cxn.terminate_instances([md['instance-id']]) 
+            qout.write(m)
+            if options.terminate:
+                ec2cxn.terminate_instances([md['instance-id']]) 
 
 
 
